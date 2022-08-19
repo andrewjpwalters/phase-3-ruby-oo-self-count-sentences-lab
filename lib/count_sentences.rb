7f0be2477455e76bc4ac95cbe8e5aabc2ccccc75
Andrew Walters <andrewjpwalters@gmail.com>
2022-08-18 22:45:12 -0500

    commit tree 89834a3bacc17932433ca5bc907157639838f8f5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,25 @@ require 'pry'
 
 class String
 
+  def report_on_self
+    "Self is #{self}"
+  end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
+
+binding.pry
